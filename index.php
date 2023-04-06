7ff1d73b71588e48ab78cca58ca3fd9808c74412
jaronavittan <96601217+jaronavittan@users.noreply.github.com>
2023-04-06 16:17:12 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="functions.php" method="get">
+     <input type="text" name="num01" placeholder="Number 1">
+     <select name="oper">
+       <label>Choose operation!</label>
+       <option value="add">Add</option>
+       <option value="sub">Subtract</option>
+       <option value="mult">Multiply</option>
+     </select>
+       <input type="text" name="num02" placeholder="Number 2">
+       <button type="submit">Calculate</button>
+    </form> 
+  </body>
+</html>
+
